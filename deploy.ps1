4ba6d0ebe1f592da89795caa99b7a74173582cc0
Joel Arias <162398454+joelwitt@users.noreply.github.com>
2025-02-21 21:00:29 -0300

    Zabbix install

---
+++ deploy.ps1
@@ -0,0 +1,35 @@
+﻿# Read the list of servers from a file
+$servers = Get-Content -Path "C:\path\to\servers.txt"
+$localInstallerPath = "C:\path\to\zabbix_agent2.msi"
+$scriptPath = "C:\path\to\installer.ps1"
+$zabbixServerIP = "YOUR_ZABBIX_SERVER_IP"
+
+foreach ($server in $servers) {
+    $destinationPath = "\\$server\c$\zabbix_agent2.msi"
+
+    # Create a remote PowerShell session
+    $session = New-PSSession -ComputerName $server
+
+    # Try to copy the MSI file to the remote machine
+    try {
+        Write-Output "Copying $localInstallerPath to $destinationPath"
+        Copy-Item -Path $localInstallerPath -Destination $destinationPath -Force -ErrorAction Stop
+        Write-Output "File successfully copied to $server"
+    } catch {
+        Write-Error "Error copying the file to $server : $_"
+        continue
+    }
+
+    # Set execution policy
+    Invoke-Command -Session $session -ScriptBlock { Set-ExecutionPolicy Bypass -Force }
+
+    # Check the execution policy
+    $executionPolicy = Invoke-Command -Session $session -ScriptBlock { Get-ExecutionPolicy }
+    Write-Output "Execution Policy on $server : $executionPolicy"
+
+    # Run the installation script
+    Invoke-Command -Session $session -FilePath $scriptPath -ArgumentList $zabbixServerIP
+
+    # Close the session
+    Remove-PSSession -Session $session
+}
---
+++ installer.ps1
@@ -0,0 +1,56 @@
+﻿param (
+    [string]$zabbixServerIP
+)
+
+# Check if any Zabbix service is installed
+$zabbixService = Get-Service -Name "*zabbix*" -ErrorAction SilentlyContinue
+
+if ($null -ne $zabbixService) {
+    Write-Output "Zabbix Agent is already installed."
+} else {
+    # Install the Zabbix agent
+    Start-Process msiexec.exe -ArgumentList "/i c:\zabbix_agent2.msi /quiet /norestart SERVER=$zabbixServerIP /log C:\zabbix_install.log" -Wait
+
+    # Pause for 60 seconds after installation
+    Start-Sleep -Seconds 60
+
+    # Get the FQDN of the hostname
+    $FQDN = ([System.Net.Dns]::GetHostEntry($env:COMPUTERNAME)).HostName
+
+    # Read and modify the configuration file
+    $configPath = "C:\Program Files\Zabbix Agent 2\zabbix_agent2.conf"
+
+    if (Test-Path $configPath) {
+        $configContent = Get-Content -Path $configPath
+
+        # Add logging for debugging
+        Write-Output "Original file content:"
+        Write-Output $configContent
+
+        # Modify necessary lines
+        $configContent = $configContent -replace '^#\s*Hostname\s*=\s*.*', "Hostname=$FQDN"
+
+        # Save the modified file
+        Set-Content -Path $configPath -Value $configContent
+
+        # Check the output of the modified file
+        $updatedContent = Get-Content -Path $configPath
+        Write-Output "Updated file content:"
+        Write-Output $updatedContent
+    } else {
+        # Create the file if it doesn't exist
+        Set-Content -Path $configPath -Value @"
+Hostname=$FQDN
+"@
+    }
+
+    # Start the Zabbix agent service
+    Start-Service -Name "Zabbix Agent 2"
+    Set-Service -Name "Zabbix Agent 2" -StartupType Automatic
+
+    # Add Zabbix agent exception to the firewall
+    New-NetFirewallRule -DisplayName "Zabbix Agent 2" -Direction Inbound -Program "C:\Program Files\Zabbix Agent 2\zabbix_agent2.exe" -Action Allow
+
+    # Verify installation
+    Get-Service "*Zabbix Agent*"
+}
